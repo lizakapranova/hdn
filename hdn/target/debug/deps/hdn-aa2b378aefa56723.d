51a40732027f29014f9eaf23cc6e77a493edc569
Elizabeth Kapranova <104550923+lizakapranova@users.noreply.github.com>
2022-12-21 15:47:12 +0300

    Add files via upload

---
+++ hdn/target/debug/deps/hdn-aa2b378aefa56723.d
@@ -0,0 +1,5 @@
+/Users/elizabethkapranova/Desktop/Rust/hdn/target/debug/deps/hdn-aa2b378aefa56723.rmeta: src/main.rs
+
+/Users/elizabethkapranova/Desktop/Rust/hdn/target/debug/deps/hdn-aa2b378aefa56723.d: src/main.rs
+
+src/main.rs:
---
+++ hdn/target/debug/deps/hdn-f9e58dd0b4d67fba.d
@@ -0,0 +1,5 @@
+/Users/elizabethkapranova/Desktop/Rust/hdn/target/debug/deps/hdn-f9e58dd0b4d67fba.rmeta: src/lib.rs
+
+/Users/elizabethkapranova/Desktop/Rust/hdn/target/debug/deps/hdn-f9e58dd0b4d67fba.d: src/lib.rs
+
+src/lib.rs:
